41583a17420e8176a5817d9d714b2bb2f68b5b86
McLovin3 <mathieu.ford@gmail.com>
2022-05-05 19:58:19 -0400

    Removed Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,12 @@ public class PlayerMovement : MonoBehaviour
     public Transform cameraTransform;
     public float gravity;
     public float movementSpeed;
-    public float turnTime;
+    public float rotationTurnTime;
     public float sprintSpeedMultiplier;
-    public float jumpHeight;
 
     private bool sprinting;
     private float currentMovementSpeed;
-    private float currentVelocity;
+    private float currentRotationVelocity;
 
     void Start()
     {
@@ -22,6 +21,33 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Update()
+    {
+        //Apply gravity
+        controller.Move(Vector3.up * gravity * Time.deltaTime);
+
+        CheckIfSprinting();
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
+
+        if (direction != Vector3.zero)
+        {
+            //Finds the vector between x and z (Atan2), turns it into degrees and then add camera rotation to move with the camera
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            //Smoothens rotation change
+            float playerAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentRotationVelocity, rotationTurnTime);
+            transform.rotation = Quaternion.Euler(0f, playerAngle, 0f);
+
+            //Turns rotation into x and z
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * Time.deltaTime * currentMovementSpeed);
+        }
+    }
+
+    private void CheckIfSprinting()
     {
         if (Input.GetKey(KeyCode.LeftShift) && !sprinting)
         {
@@ -33,28 +59,5 @@ public class PlayerMovement : MonoBehaviour
             currentMovementSpeed = movementSpeed;
             sprinting = false;
         }
-
-        float jumpVelocity = 0f;
-        if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            jumpVelocity = jumpHeight * -gravity;
-        }
-
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
-
-        if (direction != Vector3.zero)
-        {
-            //Finds the vector between x and z (Atan2), turns it into degrees and then add camera rotation to move with the camera
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
-            float playerAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentVelocity, turnTime);
-            transform.rotation = Quaternion.Euler(0f, playerAngle, 0f);
-
-            //Turns rotation into x and z
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * Time.deltaTime * currentMovementSpeed);
-        }
-
-        //Apply gravity
-        controller.Move(Vector3.up * (gravity + jumpVelocity) * Time.deltaTime);
     }
 }
