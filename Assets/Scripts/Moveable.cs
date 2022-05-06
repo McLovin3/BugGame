2889956231583b89e4c622144f56c78c6fe60f8b
McLovin3 <mathieu.ford@gmail.com>
2022-05-06 11:27:22 -0400

    Created MoveableRock prefab

---
+++ Assets/Scripts/Moveable.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Moveable : MonoBehaviour
+{
+    public GameObject commonBug;
+    public GameObject rareBug;
+    public GameObject legendaryBug;
+    public Animation moveAnimation;
+    public float secondsToReset = 120f;
+
+    private Vector3 initPosition;
+    private bool moved = false;
+
+    void Start()
+    {
+        initPosition = transform.position;
+    }
+
+    public void Move()
+    {
+        if (!moved)
+        {
+            moved = true;
+            float randomiser = Random.Range(0, 1);
+
+            if (randomiser > 0.25f)
+            {
+                Instantiate(commonBug, transform.position, Quaternion.identity);
+
+            }
+            else if (randomiser > 0.75f)
+            {
+                Instantiate(rareBug, transform.position, Quaternion.identity);
+
+            }
+            else if (randomiser > 0.90f)
+            {
+                Instantiate(legendaryBug, transform.position, Quaternion.identity);
+            }
+
+            moveAnimation.Play();
+            Invoke("ResetMovement", secondsToReset);
+        }
+    }
+
+    private void ResetMovement()
+    {
+        transform.position = initPosition;
+        moved = false;
+    }
+}
