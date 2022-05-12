21863d5f6f9ee67c9786b30d34af18d11a28d3a7
McLovin3 <mathieu.ford@gmail.com>
2022-05-12 16:50:53 -0400

    Added gravity to bugs

--- Assets/Scripts/BugScript.cs
+++ Assets/Scripts/BugScript.cs
@@ -6,6 +6,7 @@ public class BugScript : MonoBehaviour
     public CharacterController controller;
     public float movementSpeed = 2.5f;
     public float moveWhenDistance = 10f;
+    public float gravity = 9.81f;
     public int bugId = 1;
     public int value = 0;
 
@@ -23,7 +24,7 @@ public class BugScript : MonoBehaviour
         if (Vector3.Distance(player.transform.position, transform.position) <= moveWhenDistance)
         {
             Vector3 direction = player.transform.position - transform.position;
-            direction.y = 0;
+            direction.y -= 0;
             controller.Move(direction.normalized * Time.deltaTime * -movementSpeed);
         }
     }
